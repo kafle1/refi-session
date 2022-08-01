0e6389d469f24dbef057c37ef75e95b6bdecb136
kafle1 <kafleniraj@gmail.com>
2022-08-01 22:14:10 +0545

    day 03 complete

---
+++ 03/index.js
@@ -0,0 +1,107 @@
+// int a = 6;
+// c, dart, java, c++ (staticly typed)
+
+// let a = 5;
+// let b = "name";
+// javascript, python (dynamicly typed)
+
+// let a = 5;
+// a = 7;
+
+// console.log(a);
+
+// const b = 6;
+// // b = 5;
+
+// console.log(b);
+
+// functionToAddNumber => camel casing
+
+// function-to-add-number => kebab casing
+
+// function_to_add_number => snake casing
+
+// FUNCTION_TO_ADD_NUMBER => contant casing
+
+// Objects
+// const myDetail = {
+//   name: "Niraj",
+//   age: 12,
+//   dob: 2003,
+// };
+
+// console.log(myDetail.name, myDetail.dob);
+
+// let x = 5;
+
+// x = x + 7;
+// x += 7;
+
+// console.log(x);
+
+// let firstName = "Niraj";
+// let lastName = " Kafle";
+
+// console.log(firstName + lastName);
+
+// let num = 1;
+// let name = "Niraj";
+
+// let addition = num + name;
+
+// console.log(typeof(addition));
+
+// console.log(num1 === num2);
+
+// = => assign
+// == => compare
+// === => compare + checks dataType
+
+// let num1 = 5;
+// let num2 = "5";
+
+// if (num1 === num2) {
+//     console.log("They are equall");
+// } else {
+//     console.log("They are unequall")
+// }
+
+// for (let i = 0; i < 5; i++) {
+//   console.log(i, "Taude chor");
+// }
+
+// let i = 0;
+
+// while (i < 5) {
+//   console.log(i, "Taude");
+//   i++;
+// }
+
+// let nam = 'Niraj';
+
+// console.log(nam.length);
+
+// let str = "Apple, Banana, Kiwi";
+// console.log(str.slice(7, 13));
+
+// let text = "Please visit Microsoft!";
+// let newText = text.replace("Microsoft", "W3Schools");
+
+// console.log(text);
+
+// let fName = "Niraj";
+// let lName = "Kafle";
+
+// console.log(
+//   ` My name is
+// ${fName}
+// ${lName}`
+// );
+
+// console.log ( Date() );
+
+// let num = 45345;
+
+// console.log(`num`);
+
+// console.log(typeof(num));
