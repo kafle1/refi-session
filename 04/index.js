487743dec081a35b54d5498226ee3c57d6fa2fe0
kafle1 <kafleniraj@gmail.com>
2022-08-02 22:13:01 +0545

    complete day 6

---
+++ 04/index.js
@@ -0,0 +1,90 @@
+// function welcome (name) {
+//     console.log(`My name is ${name}`);
+// }
+
+// welcome("Praful Basnet");
+// welcome("Niraj Kafle")
+
+// function add(a, b) {
+//   c = a + b;
+//   return c;
+// }
+
+// let result = add(8, 2);
+// console.log(result);
+
+// const add = (a, b) =>  a + b;
+
+// console.log(add(5, 5));
+
+// function add({ num1, num2 }) {
+//   return num1 + num2;
+// }
+
+// console.log(add({ num2: 4, num1: 5 }));
+
+// const myDetails = {
+//     name: "Niraj",
+//     age: 20,
+//     favouriteFood: 'Momo',
+//     eat: function () {
+//         console.log(`I like to eat ${this.favouriteFood}`);
+//     }
+// }
+
+// console.log(myDetails.eat());
+
+// let c = 4;
+
+// function add() {
+//   console.log(c);
+// }
+
+// add();
+// console.log(c);
+
+// const add = (b) => {
+//   let c = 8;
+//   return b + c;
+// };
+
+// c = 7;
+
+// console.log(add(8));
+
+// const myGirls = ["Cecilie", "Lone", "Emil", "Tobias", "Linus"];
+
+// const myChildren = myGirls.concat(myBoys);
+
+// console.log(myGirls);
+// console.log(myBoys);
+// console.log(myChildren);
+
+// JS + libuv = Node
+
+// JS + V8 = Browser
+// 1. form
+// 2. prompt()
+
+// setTimeout(function (){
+//     console.log('Hello');
+// }, 5000)
+
+// setInterval(function(){
+//     console.log("Hello");
+// }, 2000 )
+
+// let a = 4556366.78879;
+
+// // console.log(Math.floor(a));
+// // console.log(Math.round(a));
+// console.log(Math.round(Math.random()*100))
+
+// try {
+//   console.og("Hello");
+// } catch (error) {
+//   console.log(
+//     "Sorry there was an error, please try again later",
+//     error.message
+//   );
+// }
